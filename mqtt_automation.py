a9a03fb08d4d8849aa28d9412c856b9435aeee56
afspo <arien.0a@afsarpour.com>
2023-01-14 18:20:00 +0000

    Added basic hallway motion sensor response. No logic based on time of day.

--- mqtt_automation.py
+++ mqtt_automation.py
@@ -7,6 +7,7 @@ This is a temporary script file.
 import paho.mqtt.client as mqtt
 import json
 import pandas as pd
+import datetime
 
 # mqtt setup
 brokerAddress = '192.168.1.158'
@@ -19,26 +20,27 @@ pyClient.subscribe("zigbee2mqtt/switch_hallway", 0)
 pyClient.subscribe("zigbee2mqtt/motion_hallway", 0)
 pyClient.subscribe("zigbee2mqtt/ceilingLamp_hallway", 0)
 
+
+
 # Create functions
 # Hallway switch actuation
 def on_message_switch_hallway(pyClient, userdata, message):
-    state_switchHallway = pd.read_json(message.payload.decode("UTF-8"))    
+    state_switchHallway = pd.read_json(message.payload.decode("UTF-8"))
     if state_switchHallway['action'].iloc[0] == "on":
-        print("switch is ON")
         pyClient.publish("zigbee2mqtt/ceilingLamp_hallway/set", '{"state":"ON"}')
-        
     else:
-        print("switch is OFF")
         pyClient.publish("zigbee2mqtt/ceilingLamp_hallway/set", '{"state":"OFF"}')
 
-    
 # Hallway motion sensor activation
 def on_message_motion_hallway(pyClient, userdata, message):
-    print(json.loads(message.payload.decode("UTF-8")))
-
+    state_motionHallway = pd.read_json(message.payload.decode("UTF-8"), typ='series')
+    if state_motionHallway['occupancy'] == 1:
+        pyClient.publish("zigbee2mqtt/ceilingLamp_hallway/set", '{"state":"ON"}')
+    elif state_motionHallway['occupancy'] == 0:
+        pyClient.publish("zigbee2mqtt/ceilingLamp_hallway/set", '{"state":"OFF"}')  
 
 while True:
-    # pyClient.on_message = on_message
     pyClient.message_callback_add("zigbee2mqtt/switch_hallway", on_message_switch_hallway)
     pyClient.message_callback_add("zigbee2mqtt/motion_hallway", on_message_motion_hallway)
     pyClient.loop_forever()
+    # print(datetime.datetime.now())
