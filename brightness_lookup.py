9f21f67394c0837b262daeb659b651ed53e6c771
afspo <arien.0a@afsarpour.com>
2023-01-15 09:54:52 +0000

    Created dataframe only. No functionality.

---
+++ brightness_lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 15 09:42:38 2023
+
+@author: Arien
+"""
+import pandas as pd
+
+brightnessLookup = pd.DataFrame(columns=['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
